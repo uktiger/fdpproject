34c8203f52b77e184f7e1c726e47454e9d73b841
Aditya_Thakare <adityathakare72@gmail.com>
2024-12-27 05:19:27 +0530

    created the result.html template to view given info after submitting on webpage

--- flask-app/app.py
+++ flask-app/app.py
@@ -8,7 +8,23 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    return f"Working Go Ahead"
+    # Retrieve form data using request.form
+    form_data = {
+        "Number of Dependents": request.form.get("no_of_dependents"),
+        "Education": request.form.get("education"),
+        "Self Employed": request.form.get("self_employed"),
+        "Annual Income": request.form.get("income_annum"),
+        "Loan Amount": request.form.get("loan_amount"),
+        "Loan Term": request.form.get("loan_term"),
+        "CIBIL Score": request.form.get("cibil_score"),
+        "Residential Assets Value": request.form.get("residential_assets_value"),
+        "Commercial Assets Value": request.form.get("commercial_assets_value"),
+        "Luxury Assets Value": request.form.get("luxury_assets_value"),
+        "Bank Asset Value": request.form.get("bank_asset_value"),
+    }
+
+    # Display the collected data
+    return render_template('result.html', form_data=form_data)
 
 
 
